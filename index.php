f0f3918abb43107d11e56f3ddb7302d235e4c26a
serdaryigit <serdyigi@student.arteveldehs.be>
2017-10-11 23:16:55 +0200

    Oefening1.4
    Patronen

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+/* Patronen */
+
+echo "<h1>Patronen</h1>";
+
+    for ($i = 1; $i <10; $i++){
+        if ($i > 10) {
+            echo "<a href='patroon-{$i}.php'>Patroon {$i}</a><br>";
+        } else {
+            echo "<a href='patroon-0{$i}.php'>Patroon {$i}</a><br>";
+        }
+    }   
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Patronen</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+          integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+</head>
+<body>
+  
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
+        integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
+</html>
